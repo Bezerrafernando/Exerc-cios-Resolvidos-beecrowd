29215661ed6fc36a667b1410af7054f9e40799bf
unknown <bezerrafernando>
2023-03-04 12:31:44 -0300

    Desafios_beecrowd

---
+++ Uri_1050_DDD/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+int DDD = int.Parse(Console.ReadLine());
+
+if(DDD == 61){
+    Console.WriteLine("Brasilia");
+}
+else if(DDD == 71){
+    System.Console.WriteLine("Salvador");
+}
+else if(DDD == 11){
+    System.Console.WriteLine("São Paulo");
+}
+else if(DDD == 21){
+    System.Console.WriteLine("Rio de Janeiro");
+}
+else if(DDD == 32){
+    System.Console.WriteLine("Juiz de Fora");
+}
+else if(DDD == 19){
+    System.Console.WriteLine("Campinas");
+}
+else if(DDD == 31){
+    System.Console.WriteLine("Belo Horizonte");
+}
+else{
+    Console.WriteLine("DDD não cadastrado");
+}
