339effebed94aa7dd0b32153af43a5ea64c0e426
unknown <bezerrafernando>
2023-03-05 19:19:26 -0300

    Desafios_beecrowd

---
+++ Uri_1060_Numeros_Positivos/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+Console.Clear();
+
+double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double d = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double e = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double f = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+int cont = 0;
+if (a > 0.0)
+{
+    cont = cont + 1;
+}
+if (b > 0.0)
+{
+    cont = cont + 1;
+}
+if (c > 0.0)
+{
+    cont = cont + 1;
+}
+if (d > 0.0)
+{
+    cont = cont + 1;
+}
+if (e > 0.0)
+{
+    cont = cont + 1;
+}
+if (f > 0.0)
+{
+    cont = cont + 1;
+}
+Console.WriteLine(cont + " Valores positivos");
