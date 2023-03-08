66fa8a44fcaff58465d2ec3b3130a36129190fbd
unknown <bezerrafernando>
2023-03-08 16:49:25 -0300

    Estrutura Condicional

---
+++ Uri_1064_Positivos_e_Media/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+Console.Clear();
+
+double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double d = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double e = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double f = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+int cont =0;
+double soma = 0.0;
+if(a > 0.0){
+    cont = cont + 1;
+    soma = soma + a;
+}
+if(b > 0.0){
+    cont = cont + 1;
+    soma = soma + b;
+}
+if(c > 0.0){
+    cont = cont + 1;
+    soma = soma + c;
+}
+if(d > 0.0){
+    cont = cont + 1;
+    soma = soma + d;
+}
+if(e > 0.0){
+    cont = cont + 1;
+    soma = soma + e;
+}
+if(f > 0.0){
+    cont = cont + 1;
+    soma = soma + f;
+}
+Console.WriteLine(cont + " Valores positivos");
+
+double media = soma / cont;
+Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
---
+++ Uri_1065_Pares_entre_Cinco_Numeros/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int d = int.Parse(Console.ReadLine());
+int e = int.Parse(Console.ReadLine());
+
+int Cont = 0;
+if(a % 2 == 0){
+    Cont = Cont + 1;
+}
+if(b % 2 == 0){
+    Cont = Cont + 1;
+}
+if(c % 2 == 0){
+    Cont = Cont + 1;
+}
+if(d % 2 == 0){
+    Cont = Cont + 1;
+}
+if(e % 2 == 0){
+    Cont = Cont + 1;
+}
+
+Console.WriteLine(Cont + " valores pares");
+
