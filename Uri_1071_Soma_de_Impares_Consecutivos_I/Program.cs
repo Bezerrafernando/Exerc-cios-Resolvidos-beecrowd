09b360149cb3443afb14047243b7d3d9f07ae50f
bezerrafernando <fernandodbezerra@gmail.com>
2023-04-03 18:25:08 -0300

    Estrutura de repetição para

---
+++ Uri_1071_Soma_de_Impares_Consecutivos_I/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+
+int min, max;
+if(x < y){
+    min = x;
+    max = y;
+}
+else {
+    min = y;
+    max = x;
+}
+int soma = 0;
+for(int i = min + 1; i < max; i++){
+    if (i % 2 != 0){
+        soma = soma + i;
+    }
+}
+Console.WriteLine(soma);
---
+++ Uri_1078_Tabuada/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uri1078{
+    class Program{
+        static void Main(string[]agrs){
+            Console.Clear();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i <= 10; i++){
+                int resultado = i * n;
+                Console.WriteLine(i + " x " + n + " = " + resultado );
+            }
+        }
+    }
+}
