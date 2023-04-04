189bbde03029ed55b1c7b407f68b7b6db97bde25
bezerrafernando <fernandodbezerra@gmail.com>
2023-04-04 16:43:37 -0300

    Estruturas de Repetição for

---
+++ Uri_1067_Numeros_Impares/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+int x = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= x; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+}
---
+++ Uri_1073_Quadrado_de_Pares/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= n; i++)
+{
+    if (i % 2 == 0)
+    {
+        int quadrado = i * i;
+        Console.WriteLine(i + "^2 = " + quadrado);
+    }
+}
